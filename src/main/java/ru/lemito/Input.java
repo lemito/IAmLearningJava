86b9390080f02d12ae38bae553cc5faf6b654bb9
Michael Tropin <mihtrop2005@yandex.ru>
2024-07-13 23:23:02 +0300

    Add input from string

--- src/main/java/ru/lemito/Input.java
+++ src/main/java/ru/lemito/Input.java
@@ -1,23 +1,23 @@
 package ru.lemito;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
+        console.useLocale(Locale.US); 
         String name = console.nextLine();
         int age = console.nextInt();
-        System.out.println("Hello, " + name + ", your age = " + age);
+        double gpa = console.nextDouble();
+        System.out.println("Hello, " + name + ", your age = " + age + ", gpa = " + gpa);
 
-        String str = "25.52 55.48 9";
+        String str = "19 4.87";
         Scanner fromStr = new Scanner(str);
-        boolean check1 = fromStr.hasNextDouble();
-//        double first = fromStr.nextDouble();
-        boolean check2 = fromStr.hasNextDouble();
-//        double second = fromStr.nextDouble();
-//        int third = fromStr.nextInt();
-//        System.out.println((first + second) / third);
-        System.out.println(String.valueOf(check1) + check2);
+        fromStr.useLocale(Locale.US); 
+        int age_fromStr = fromStr.nextInt();
+        double gpa_fromStr = fromStr.nextDouble();
+        System.out.println(", your age = " + age_fromStr + ", gpa = " + gpa_fromStr);
     }
 }
